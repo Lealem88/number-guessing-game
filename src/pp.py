fa3adc0f8a84746ee161f2d06f9f9b13750e70cb
laliproj <lalitechzone@gmail.com>
2023-04-14 10:49:00 +0300

    Add logic for guesses that are too high

--- src/pp.py
+++ src/pp.py
@@ -7,3 +7,7 @@ def main(): #function
     while True:
         guess = int(input("Guess a number between 1 and 10: "))
 
+        if guess > number:
+            print("Too high")
+            continue
+
