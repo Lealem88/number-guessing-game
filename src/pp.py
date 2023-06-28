8ebdacbcd0ba73b97910d333f3b33c61c669b7c5
laliproj <lalitechzone@gmail.com>
2023-06-28 22:35:00 +0300

    Add contributor names

--- src/pp.py
+++ src/pp.py
@@ -1,25 +1,17 @@
 import random
-
 def main(): #function
-
     number = random.randint(1, 10)
-
     while True:
         guess = int(input("Guess a number between 1 and 10: "))
-
         if guess > number:
             print("Too high")
             continue
-
         if guess < number:
             print("Too low")
             continue
-
         else:
             print("Correctly guessed")
             break
 
-
 if __name__ == "__main__":
     main()
-
