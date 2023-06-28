755c941ebc9e94053cbe51577251d66fa90b01ea
laliproj <lalitechzone@gmail.com>
2023-06-28 15:21:00 +0300

    Implement Java loop logic

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,10 @@ public class Main {
 
         System.out.printf("Hello and welcome!");
 
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
+
